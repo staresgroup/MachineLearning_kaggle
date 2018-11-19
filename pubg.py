e80de2505ebf65cc160ba8e86506251bcae723ad
staresgroup <42593585+staresgroup@users.noreply.github.com>
2018-11-19 23:56:32 +0800

    new score is 0.650

--- pubg.py
+++ pubg.py
@@ -3,80 +3,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import preprocessing
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegression
+from ultimate.mlp import MLP
+import time
+import gc
+gc.enable()
 
 
-def load_data(file_path):
+def load_data(file_path, dtypes):
     print('loading data......')
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, dtype=dtypes)
     df = df.dropna()
     print('loaded finally')
     return df
 
 
-def wash_data(X_train, y_train):
-        print(X_train.head())
-        print(X_train.info())
-        print(X_train.isnull().any())
-        print(y_train.isnull().any())
-        # X_train['DBNOs'].replace([0], X_train['DBNOs'].mean())
-        # X_train['assists'].replace([0], X_train['assists'].mean())
-        # X_train['boosts'].replace([0], X_train['boosts'].mean())
-        # X_train['damageDealt'].replace([0], X_train['damageDealt'].mean())
-        # X_train['headshotKills'].replace([0], X_train['headshotKills'].mean())
-        # X_train['killStreaks'].replace([0], X_train['killStreaks'].mean())
-        # X_train['kills'].replace([0], X_train['kills'].mean())
-        # X_train['longestKill'].replace([0], X_train['longestKill'].mean())
-        # X_train['roadKills'].replace([0], X_train['roadKills'].mean())
-        # X_train['weaponsAcquired'].replace([0], X_train['weaponsAcquired'].mean())
-        # X_train['maxPlace'].replace([0], X_train['maxPlace'].mean())
-        # y_train['winPlacePerc'].replace([0], y_train['winPlacePerc'].mean())
-        print(X_train.isnull().any())
-
-
-def train_LR_model(X_train, y_train):
-    linear_model = LinearRegression()
-    linear_model.fit(X_train, y_train)
-    return linear_model
-
-
-def predict(model, X_test):
-    print('predicting......')
-    predict = model.predict(X_test)
-    return predict
-
-
-def save_csv(predict, test):
+def save_csv(pred, test):
     print('saving......')
-    pd_data = pd.DataFrame(predict)
+    pd_data = pd.DataFrame(pred)
     pd_data.columns = ['winPlacePerc']
     pd_data = pd.concat([test['Id'], pd_data], axis=1)
     pd_data.to_csv('submission.csv', index=False)
-
-
-def normalized(X_train):
-    X_train['DBNOs'] = (X_train['DBNOs'] - X_train['DBNOs'].min()) / (X_train['DBNOs'].max() - X_train['DBNOs'].min())
-    return X_train
+    print('saved OK')
 
 
 if __name__ == '__main__':
-    features = ['DBNOs', 'assists', 'boosts', 'damageDealt', 'headshotKills', 'killStreaks', 'kills', 'longestKill',
-                'roadKills', 'weaponsAcquired', 'maxPlace']
+    start_time = time.time()
+    features = ['assists', 'boosts', 'damageDealt', 'DBNOs', 'headshotKills', 'heals', 'killPlace', 'killPoints',
+                'kills', 'killStreaks', 'longestKill', 'maxPlace', 'revives', 'roadKills', 'weaponsAcquired',
+                'rideDistance', 'swimDistance', 'walkDistance']
+    dtypes = {
+        'Id': 'object',
+        'groupId': 'object',
+        'matchId': 'object',
+        'assists': 'int8',
+        'boosts': 'int8',
+        'damageDealt': 'float32',
+        'DBNOs': 'int8',
+        'headshotKills': 'int8',
+        'heals': 'int8',
+        'killPlace': 'int8',
+        'killPoints': 'int16',
+        'kills': 'int8',
+        'killStreaks': 'int8',
+        'longestKill': 'float32',
+        'matchDuration': 'int16',
+        'matchType': 'object',
+        'maxPlace': 'int16',
+        'numGroups': 'int16',
+        'revives': 'int8',
+        'rideDistance': 'float32',
+        'roadKills': 'int8',
+        'swimDistance': 'float32',
+        'teamKills': 'int16',
+        'vehicleDestroys': 'int8',
+        'walkDistance': 'float32',
+        'weaponsAcquired': 'int16',
+        'winPoints': 'int32',
+    }
     features_target = ['winPlacePerc']
-    train = load_data('../input/train_V2.csv')
-    test = load_data('../input/test_V2.csv')
+    train = load_data('../input/train_V2.csv', dtypes)
+    test = load_data('../input/test_V2.csv', dtypes)
+    print(train.info())
     X_train = train[features]
     y_train = train[features_target]
     X_test = test[features]
-    # wash_data(X_train, y_train)
-    # X_train = normalized(X_train)
-    # X_test = normalized(X_test)
-    X_train = (X_train -X_train.mean()) / X_train.std()
-    X_test = (X_test - X_test.mean()) / X_test.std()
-    y_train = (y_train - y_train.mean()) / y_train.std()
-    # lab_enc = preprocessing.LabelEncoder()
-    # y_train = lab_enc.fit_transform(y_train)
-    model = train_LR_model(X_train, y_train)
-    predict = predict(model, X_test)
-    save_csv(predict, test)
+    del train
+    gc.collect()
+    X_train['totalDistance'] = X_train['walkDistance'] + X_train['rideDistance'] + X_train['swimDistance']
+    X_test['totalDistance'] = X_test['walkDistance'] + X_test['rideDistance'] + X_test['swimDistance']
+    del X_train['walkDistance'], X_train['rideDistance'], X_train['swimDistance']
+    del X_test['walkDistance'], X_test['rideDistance'], X_test['swimDistance']
+    gc.collect()
+    scaler = preprocessing.MinMaxScaler(copy=False).fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    mlp = MLP(layer_size=[X_train.shape[1], 8, 8, 8, 1], rate_init=0.02, loss_type="mse", epoch_train=10, epoch_decay=10,
+              verbose=1)
+    mlp.fit(X_train, y_train)
+    pred = mlp.predict(X_test)
+    save_csv(pred, test)
+    end_time = time.time()
+    print('The total of time is', end_time - start_time)
+
+
